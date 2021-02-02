f2f7ef49ba93a8095e39535aef1f27347e7e7259
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-02-02 11:56:16 +0100

    closed #1

--- src/com/programacion/Braiskiskos.java
+++ src/com/programacion/Braiskiskos.java
@@ -3,6 +3,7 @@ package com.programacion;
 public class Braiskiskos {
 
     public void amosar(){
-        System.out.println(" cerrando 4 pruebas issues");
+        System.out.println(" cerrando 5 pruebas issues");
+        //close
     }
 }
